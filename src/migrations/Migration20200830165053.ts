1c708086679abcc999c75cf3f02ad06ddb14e365
v.tyalo <bobtial@inbox.ru>
2020-08-30 19:57:06 +0300

    initial crud setup

---
+++ src/migrations/Migration20200830165053.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20200830165053 extends Migration {
+
+  async up(): Promise<void> {
+
+  }
+
+}
---
+++ src/migrations/Migration20200830165116.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20200830165116 extends Migration {
+
+  async up(): Promise<void> {
+
+  }
+
+}
